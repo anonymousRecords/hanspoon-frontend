69763235ba63eabff9eaef2d5f99b70bfd430ca7
anonymousRecords <a.Records2022@gmail.com>
2025-12-08 13:41:25 +0900

    feat: ::highlight를 사용하여 선택된 텍스트에 하이라이트 기능 구현

--- apps/extension/entrypoints/content.ts
+++ apps/extension/entrypoints/content.ts
@@ -1,6 +1,34 @@
 export default defineContentScript({
-  matches: ['*://*.google.com/*'],
-  main() {
-    console.log('Hello content.');
-  },
+	matches: ["<all_urls>"],
+
+	main(ctx) {
+		const style = document.createElement("style");
+		style.textContent = `
+      ::highlight(hanspoon-marker) {
+        background-color: #E9D2FD;
+        color: black;
+      }
+    `;
+		document.head.appendChild(style);
+
+		function selectText() {
+			const selection = window.getSelection();
+			if (!selection || selection.rangeCount === 0) return;
+
+			const range = selection.getRangeAt(0);
+
+			const highlight = new Highlight(range);
+
+			CSS.highlights.set("hanspoon-marker", highlight);
+
+			selection.removeAllRanges();
+		}
+
+		document.addEventListener("mouseup", selectText);
+
+		ctx.onInvalidated(() => {
+			document.removeEventListener("mouseup", selectText);
+			style.remove();
+		});
+	},
 });
