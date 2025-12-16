e6047da0bb1e419ea37c28759144242cc05cfdae
anonymousRecords <a.Records2022@gmail.com>
2025-12-16 15:03:09 +0900

    chore: 메세징 구조 설정

---
+++ apps/extension/utils/message.ts
@@ -0,0 +1,19 @@
+import { defineExtensionMessaging } from "@webext-core/messaging";
+import type {
+	LocalAnnotation,
+	LocalPost,
+	SerializedHighlight,
+} from "@/lib/highlight/types";
+
+export interface DBProtocolMap {
+	DB_SAVE_HIGHLIGHT: (data: { data: SerializedHighlight; postId: string }) => {
+		success: boolean;
+	};
+	DB_DELETE_HIGHLIGHT: (data: { id: string }) => { success: boolean };
+	DB_GET_ALL_HIGHLIGHTS: () => LocalAnnotation[];
+	DB_GET_POST_BY_ID: (data: { postId: string }) => LocalPost | undefined;
+	DB_ADD_POST: (data: { postData: LocalPost }) => { success: boolean };
+}
+
+export const { sendMessage, onMessage } =
+	defineExtensionMessaging<DBProtocolMap>();
