9093eeddde3fdba99029d4417f8db6a758dea17b
anonymousRecords <a.Records2022@gmail.com>
2025-12-29 15:20:54 +0900

    feat: 프로필 & 수정 페이지에서 포스트 그리드 적용

--- apps/viewer/src/pages/profile/[id]/edit.tsx
+++ apps/viewer/src/pages/profile/[id]/edit.tsx
@@ -1,17 +1,10 @@
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import { useParams } from "react-router";
 import { BaseLayout } from "../../../components/layout/BaseLayout";
 import { PostGrid } from "../../../components/share/PostGrid";
 import { Profile } from "../../../components/share/Profile";
 
-export const ProfilePage = () => {
-	const { username } = useParams<{ username: string }>();
-	if (username === undefined) {
-		console.warn("username is undefined");
-		return null;
-	}
-
+export const ProfileEditPage = () => {
 	return (
 		<ErrorBoundary fallback={<div>error</div>}>
 			<BaseLayout>
@@ -19,15 +12,7 @@ export const ProfilePage = () => {
 					<Profile />
 				</Suspense>
 				<section style={{ width: "820px" }}>
-					<div
-						style={{
-							display: "grid",
-							gridTemplateColumns: "1fr 1fr 1fr",
-							gap: "12px",
-						}}
-					>
-						<PostGrid isEditable={false} />
-					</div>
+					<PostGrid isEditable={true} />
 				</section>
 			</BaseLayout>
 		</ErrorBoundary>
