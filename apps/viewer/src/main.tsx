f06025c375403fc4a7d8c6edf5a29d340feb33ea
anonymousRecords <a.Records2022@gmail.com>
2026-01-28 17:54:33 +0900

    chore: viewer에서 불필요한 파일 제거

--- apps/viewer/src/main.tsx
+++ apps/viewer/src/main.tsx
@@ -2,15 +2,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes, useParams } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
 import AuthCallback from "./components/AuthCallback.tsx";
-import { ProtectedRoute } from "./components/auth/ProtectedRoute.tsx";
 import "./index.css";
-import { ProfileEditPage } from "./pages/profile/[id]/edit.tsx";
-import { ProfilePage } from "./pages/profile/[id]/index.tsx";
-import { SharedPage } from "./pages/SharedPage.tsx";
-import SharePage from "./pages/SharePage.tsx";
+import { SharePage } from "./pages/SharePage.tsx";
 
 const queryClient = new QueryClient();
 
@@ -23,26 +19,7 @@ if (rootElement) {
 					<Routes>
 						<Route path="/" element={<App />} />
 						<Route path="/auth/callback" element={<AuthCallback />} />
-
-						<Route element={<ProtectedRoute />}>
-							<Route path="/edit" element={<ProfileEditPage />} />
-						</Route>
-
-						<Route
-							element={
-								<ProtectedRoute
-									condition={(user) => {
-										const { username } = useParams<{ username: string }>();
-										return user.user_metadata?.name === username;
-									}}
-								/>
-							}
-						>
-							<Route path="/:username" element={<ProfilePage />} />
-						</Route>
-
 						<Route path="/share/:shareId" element={<SharePage />} />
-						<Route path="/shared/:shareId" element={<SharedPage />} />
 					</Routes>
 				</BrowserRouter>
 				<ReactQueryDevtools initialIsOpen={true} />
