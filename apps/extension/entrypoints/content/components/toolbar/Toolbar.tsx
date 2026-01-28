adc5cb98bf3d5c64932e7068748c936d40aaed2e
anonymousRecords <a.Records2022@gmail.com>
2026-01-28 17:13:58 +0900

    chore: 비활성화 시, 툴바 미노출하도록 수정

--- apps/extension/entrypoints/content/components/toolbar/Toolbar.tsx
+++ apps/extension/entrypoints/content/components/toolbar/Toolbar.tsx
@@ -1,9 +1,17 @@
 import { useTextSelection } from "@/hooks/useTextSelection";
+import { useFloatingButtonStatus } from "../../hooks/useFloatingButtonStatus";
 import { CreationModeUI } from "./CreationModeUI";
 import { EditModeUI } from "./EditModeUI";
 
 export const Toolbar = () => {
 	const { clientRect, isCollapsed, range } = useTextSelection();
+	const { isEnabledForCurrentSite } = useFloatingButtonStatus();
+
+	const enabled = isEnabledForCurrentSite();
+
+	if (!enabled) {
+		return null;
+	}
 
 	if (clientRect === undefined) {
 		return null;
